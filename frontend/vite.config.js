66db015a960dea94904a0f8b2410b29bb2fd42a1
Jason Zheng <jasonzheng0814@gmail.com>
2025-08-07 16:13:22 -0400

    initial front-end setup

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001",
+        changeOrigin: true,
+      },
+    },
+  },
+});

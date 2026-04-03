d6da4f7b6fdd41772dc89891e8ea9478914a954f
Jason Zheng <jasonzheng0814@gmail.com>
2026-04-03 12:49:28 -0400

    minor change

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB successfully connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err.message);
     process.exit(1);
